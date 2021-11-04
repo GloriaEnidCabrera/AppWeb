34fa4ba77fbf8b83b783d11dd79bceb1b87f0e8e
Uruanth <82775623+Uruanth@users.noreply.github.com>
2021-11-03 19:25:39 -0500

    Funcion login
    Se agrego la validación basica del login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+
+
 
 @NgModule({
   declarations: [
@@ -12,6 +15,7 @@ import { LoginComponent } from './login/login.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,14 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  private usuarios: string[]=['admin', 'admin123'];
+  private usuarios: string[] = ['admin', 'admin123'];
+  msjProceso: boolean = false;
+
+  msjSesion: string = "Inicio correcto";
+
+  usuario: string = "";
+  password: string = "";
   
 
-  constructor() { 
-    console.log(this.usuarios)
+  constructor() {
   }
 
   ngOnInit(): void {
   }
+
+
+  login() {
+
+    this.msjProceso=true;
+    if(this.usuario !== this.usuarios[0] || this.password !== this.usuarios[1]){
+      this.msjSesion="Inicio de sesion incorrecto";
+    }else{
+      this.msjSesion="Inicio correcto";
+    }
+    console.log(this.usuario)
+    console.log(this.password)
+  }
+
 
 }
