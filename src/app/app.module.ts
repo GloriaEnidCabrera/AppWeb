78d0e332aebe576077f848fa2c17e2312962947c
Seijux <seijudt@gmail.com>
2021-11-10 23:17:08 -0500

    Diseño Footer
    Diseño de footer, funcional.
    Se agregó la etiqueta para campos vacíos.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { ProductosComponent } from './productos/productos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NavbarComponent,
+    FooterComponent,
+    ProductosComponent
   ],
   imports: [
     BrowserModule,
