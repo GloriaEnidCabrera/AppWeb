03c243040ddce652bc654911311aa1d06cec976e
Seijux <seijudt@gmail.com>
2021-11-22 12:20:01 -0500

    CorreccionCargaArchivo

--- src/app/pages/productos/file-upload.service.ts
+++ src/app/pages/productos/file-upload.service.ts
@@ -19,7 +19,17 @@ export class FileUploadService {
 
   // Retorna un objeto observable
   upload(file: any): Promise<any[]> {
+    
     return new Promise<any[]>((resolve, reject) => {
+      //eliminar todo antes de agregar
+      this.http.delete<any>(
+        this.baseApiUrl,{ observe: 'response' }).subscribe(
+          (response: any) => {
+            let resaux = [];
+            resaux[0] = response.status;
+            this.resultados.push(resaux);
+          }
+        );
       //leyendo el contenido
       var reader = new FileReader();
       reader.onloadend = (e) => {
