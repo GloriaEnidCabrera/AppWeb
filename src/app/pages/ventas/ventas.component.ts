8984bcfbcb4dbb63e1d33b891cdf991c7ec35ea8
Claudia De Young <90992201+clavg6@users.noreply.github.com>
2021-11-27 13:17:33 -0500

    CambiosEsteticaFrom
    color letra del snow market, colores en campana y lupa, fondo imagen colres sidebar, colores login. etc

--- src/app/pages/ventas/ventas.component.ts
+++ src/app/pages/ventas/ventas.component.ts
@@ -5,6 +5,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './ventas.component.html',
   styleUrls: ['./ventas.component.css']
 })
+
 export class VentasComponent implements OnInit {
 
   constructor() { }
