d4b8deecbd981f22a904db8408e3f5360942a0ff
Seijux <seijudt@gmail.com>
2021-12-09 12:51:10 -0500

    BosquejoConsolidado

--- src/app/pages/consolidacion/consolidacion.component.ts
+++ src/app/pages/consolidacion/consolidacion.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { VentasService } from '../ventas/ventas.service';
+import { ToastrService } from 'ngx-toastr';
+import { Subject, throwError } from 'rxjs';
+
 
 @Component({
   selector: 'app-consolidacion',
@@ -7,9 +11,72 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConsolidacionComponent implements OnInit {
 
-  constructor() { }
+  dtOptions: DataTables.Settings = {};
+  dtTrigger: Subject<any> = new Subject<any>();
+
+  contenido: any;
+  res: any;
+
+
+  constructor(private toastr: ToastrService, private pedidosale: VentasService) { }
 
   ngOnInit(): void {
+    
+    try {
+      this.res = this.pedidosale.getSale();
+      
+      this.res.subscribe(
+        (response: any) => {
+          this.contenido = response.body;
+          console.log(this.contenido);
+
+          this.dtTrigger.next(this.dtOptions);
+        });
+    } catch (e) {
+      console.error("BK DOWN")
+      this.contenido = []   
+    }
+
+    //Opciones especiales de la tabla, localización y caracteristicas
+    this.dtOptions = {
+      pagingType: 'full_numbers',
+      columns: [/*{
+        //title: 'ID',
+      //},*/ {
+        title: 'Ciudad',
+      }, {
+        title: 'Valor Total Ventas',
+      }],
+      pageLength: 10,
+      responsive: true,
+      language: {
+        processing: "Procesando...",
+        search: "Buscar:",
+        lengthMenu: "Mostrar _MENU_ elementos",
+        info: "Mostrando desde _START_ al _END_ de _TOTAL_ elementos",
+        infoEmpty: "Mostrando ningún elemento.",
+        infoFiltered: "(filtrado _MAX_ elementos total)",
+        infoPostFix: "",
+        loadingRecords: "Cargando registros...",
+        zeroRecords: "No se encontraron registros",
+        emptyTable: "No hay datos disponibles en la tabla",
+        paginate: {
+          first: "Primero",
+          previous: "Anterior",
+          next: "Siguiente",
+          last: "Último"
+        },
+        aria: {
+          sortAscending: ": Activar para ordenar la tabla en orden ascendente",
+          sortDescending: ": Activar para ordenar la tabla en orden descendente"
+        }
+      }
+    };
+
   }
 
+  //eliminando objeto revisor de cambios de la tabla
+  ngOnDestroy(): void {
+    this.dtTrigger.unsubscribe();
+  }
 }
